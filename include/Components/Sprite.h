2631b1768d63a020e242fb938a6bd1f2db326c4b
Erik Stevenson <eriknstevenson@gmail.com>
2017-10-12 21:57:59 -0400

    Add sprite animation and example of usage. Condense sprites to single texture.

--- include/Components/Sprite.h
+++ include/Components/Sprite.h
@@ -4,19 +4,61 @@
 
 #include "Component.h"
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Clock.hpp>
 
 namespace tjg {
 
     class Sprite : public Component {
     private:
-        sf::Sprite sprite;
+        // Contains the SFML sprite object(s)
+        std::vector<sf::Sprite> frames;
+
+        // Tracking animation
+        uint current_frame = 0;
+        sf::Clock animation_clock;
+        // time between frames, in milliseconds
+        int frame_delay;
+        bool playing = false;
+        bool loop = true;
+
+        // Used by SpriteRenderSystem to order sprites (lower values are drawn before higher values)
         int layer;
     public:
         // Constructor
-        explicit Sprite(sf::Sprite sprite, const int layer = 0);
+        explicit Sprite(sf::Sprite, int layer = 0);
+        Sprite(std::vector<sf::Sprite> frames, int frame_delay, int layer = 0);
+
         // Sprite methods
+        /**
+         * @return the sprite associated with the current frame of animation
+         */
         sf::Sprite & GetSprite();
+
+        /**
+         * @param frame of animation desired
+         * @return the sprite associated with the specified frame of animation
+         */
+        sf::Sprite & GetSprite(uint frame);
+
+        /**
+         * @return the SFML bounds for the current frame of animation
+         */
         sf::FloatRect GetBounds();
+
+        /**
+         * Play the animation
+         * @param loop whether the animation should loop or stop after playing once.s
+         */
+        void Play(bool loop = true);
+
+        /**
+         * Pause the animation.
+         */
+        void Pause();
+
+        /**
+         * @return the sprite's sorting layer
+         */
         int GetLayer();
     };
 }
--- src/Components/Sprite.cpp
+++ src/Components/Sprite.cpp
@@ -3,18 +3,58 @@
 namespace tjg {
 
     Sprite::Sprite(sf::Sprite sprite, const int layer) {
-        this->sprite = sprite;
+        // Centers the sprite origin.
+        auto bounds = sprite.getLocalBounds();
+        sprite.setOrigin(bounds.width / 2.0f, bounds.height / 2.0f);
+
+        frames = std::vector<sf::Sprite> {sprite};
         this->layer = layer;
-        auto bounds = this->sprite.getLocalBounds();
-        this->sprite.setOrigin(bounds.width / 2.0f, bounds.height / 2.0f);
+    }
+
+    Sprite::Sprite(std::vector<sf::Sprite> frames, const int frame_delay, const int layer) {
+        this->frames = frames;
+        this->layer = layer;
+        this->frame_delay = frame_delay;
+        // Center each frame on the origin
+        for (auto frame : this->frames) {
+            auto bounds = frame.getLocalBounds();
+            frame.setOrigin(bounds.width / 2.0f, bounds.height / 2.0f);
+        }
     }
 
     sf::Sprite &Sprite::GetSprite() {
-        return sprite;
+        return GetSprite(current_frame);
+    }
+
+    sf::Sprite &Sprite::GetSprite(const uint frame) {
+        if (playing) {
+            // check if clock has passed the frame time
+            if (animation_clock.getElapsedTime().asMilliseconds() > frame_delay) {
+                // Advance the frame by one
+                current_frame++;
+                // loop back to the beginning if necessary, and stop if not looping
+                if (current_frame > frames.size() - 1) {
+                    playing = loop;
+                    current_frame = 0;
+                }
+                animation_clock.restart();
+            }
+        }
+        return frames[frame];
+    }
+
+    void Sprite::Play(const bool loop) {
+        playing = true;
+        this->loop = loop;
+        animation_clock.restart();
+    }
+
+    void Sprite::Pause() {
+        playing = false;
     }
 
     sf::FloatRect Sprite::GetBounds() {
-        return sprite.getGlobalBounds();
+        return frames[current_frame].getGlobalBounds();
     }
 
     int Sprite::GetLayer() {
