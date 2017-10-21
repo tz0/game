77aacebd72c8bed44e1ad177e0ee2a5e1d50e0b8
Erik Stevenson <eriknstevenson@gmail.com>
2017-10-21 13:29:16 -0400

    Update documentation of sensor shape

--- include/Components/SensorShape.h
+++ include/Components/SensorShape.h
@@ -12,13 +12,12 @@ namespace tjg {
 
     /**
      * SensorShape can be used to associate a Chipmunk shape with a callback to be used for shape queries.
-     * The physics system performs a shape query for each entity containing this component, and will provide a pointer
-     * to the parent entity through the third argument of the callback (void*).
+     * The physics system performs a shape query for each entity containing this component and passes a pointer to each
+     * shape to the callback.
      *
      * A simple example:
      *
-     * my_entity->AddComponent<SensorShape>(my_entity->GetComponent<DynamicBody>->GetShape(), [](cpShape *shape, cpContactPointSet *points, void *entity_ptr){
-     *      auto entity = static_cast<Entity*>(entity_ptr);
+     * my_entity->AddComponent<SensorShape>(my_entity->GetComponent<DynamicBody>->GetShape(), [&](cpShape *shape){
      *      std::cout << "sensed a shape." << std::endl;
      * }
      */
