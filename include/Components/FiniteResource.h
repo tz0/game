38b91324bfc95789bea341c2908ffd357e241402
Cullen Rombach <cullenr@imagemattersllc.com>
2017-10-24 21:37:47 -0400

    Add FiniteResource component

---
+++ include/Components/FiniteResource.h
@@ -0,0 +1,16 @@
+//
+// Created by Cullen on 10/24/2017.
+//
+
+#ifndef GAME_FINITERESOURCE_H
+#define GAME_FINITERESOURCE_H
+
+
+
+class FiniteResource {
+
+};
+
+
+
+#endif //GAME_FINITERESOURCE_H
---
+++ src/Components/FiniteResource.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cullen on 10/24/2017.
+//
+
+#include "FiniteResource.h"
