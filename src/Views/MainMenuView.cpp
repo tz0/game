e3753c3b8119c191239f9d844a59bf9c676b0ca2
hyy369 <hyy369@users.noreply.github.com>
2017-10-25 23:32:42 -0400

    implement main menu skeleton

--- src/Views/MainMenuView.cpp
+++ src/Views/MainMenuView.cpp
@@ -6,7 +6,17 @@ namespace tjg {
 
 
     void MainMenuView::Initialize() {
-
+        auto avenir_bold = resource_manager.LoadFont("Avenir-Bold.ttf");
+        // temp Set font for win message
+        message.setFont(*avenir_bold);
+        // Create a win message.
+        message.setStyle(sf::Text::Bold);
+        message.setCharacterSize(24);
+        message.setString("Press ENTER/RETURN to start!");
+        // Center the win message on the screen.
+        sf::FloatRect textRect = message.getLocalBounds();
+        message.setOrigin(textRect.left + (textRect.width / 2), textRect.top + (textRect.height / 2));
+        message.setPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT/ 2);
     }
 
     //Handle keyboard control of the menu
@@ -16,10 +26,28 @@ namespace tjg {
 
 
     void MainMenuView::Render() {
-
+        window.setView(window.getDefaultView());
+        window.clear(sf::Color(50, 50, 50, 255));
+        window.draw(message);
+        window.display();
     }
 
     void MainMenuView::HandleWindowEvents(sf::Event event) {
-
+        switch (event.type) {
+            case sf::Event::KeyPressed: {
+                switch (event.key.code) {
+                    // Toggle FPS counter on F1.
+                    case sf::Keyboard::Return: {
+                        //TODO::Switch to player view
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                break;
+            }
+            default:
+                break;
+        }
     }
 }
