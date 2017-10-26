13acf35d8ca7bc1c47c8e87732a4d7fc8b41bab4
hyy369 <hyy369@users.noreply.github.com>
2017-10-25 21:04:29 -0400

    remove commented codes

--- include/View.h
+++ include/View.h
@@ -11,21 +11,15 @@ namespace tjg {
     class View {
 
     protected:
-//        LogicCenter &logic_center;
         sf::RenderWindow &window;
         ResourceManager resource_manager;
 
     public:
-//        View(LogicCenter &logic_center) : logic_center(logic_center) {};
         View(sf::RenderWindow &window, ResourceManager &resourceManager) :
                 window(window),
                 resource_manager(resourceManager){};
         virtual void Initialize() = 0;
-
-        /**
-         * @return whether the view is still running
-         */
-//        virtual bool Running() = 0;
+        virtual void HandleWindowEvents(sf::Event event) = 0;
     };
 }
 #endif //GAME_VIEW_H
