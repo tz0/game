c92ba2576c25833e545c6f0c743269323bc84ad6
hyy369 <hyy369@users.noreply.github.com>
2017-10-27 10:47:24 -0400

    Add Constants module

---
+++ include/Constants.h
@@ -0,0 +1,9 @@
+#ifndef GAME_CONSTANTS_H
+#define GAME_CONSTANTS_H
+
+namespace tjg {
+    enum class State {MAIN_MENU, LEVEL_MENU, PLAYING, PAUSED, WON, FAILED};
+    enum class ViewSwitch {CONTINUE, MAIN_MENU, LEVEL_MENU, PLAYING, RESUME, PAUSED, WON, FAILED};
+}
+
+#endif //GAME_CONSTANTS_H
